0755c7a3a97fdd385f933d7bea9683c1ecff2b1c
Cristian Raffi Cunha <cristianraffi@bol.com.br>
2019-03-31 19:27:10 -0300

    Fix Header news

--- JS/CONTROLE/controle.js
+++ JS/CONTROLE/controle.js
@@ -20,9 +20,9 @@ function left() {
         text.innerHTML = "Praesent euismod lacus nibh, quis pretium turpis finibus id."
             // text.innerHTML = "Nesta data se inicía a criação de um novo Layout para o aplicativo."
     } else {
-        data.innerHTML = "26/03/2019";
+        data.innerHTML = "31/03/2019";
         title.innerHTML = "Downlaod Android";
-        text.innerHTML = "Lançamos o site na verção de aplicativo android - <a href='https://drive.google.com/file/d/1N_cDg3erO02tWqmCT2CKle04fv13F7qa/view?usp=sharing'>Downlaod</a>"
+        text.innerHTML = "O site também está na versão de aplicativo android <a href='https://drive.google.com/file/d/1N_cDg3erO02tWqmCT2CKle04fv13F7qa/view?usp=sharing'>Downlaod</a>"
     }
 
 }
@@ -30,9 +30,9 @@ function left() {
 function right() {
 
     if (y > 0) {
-        data.innerHTML = "26/03/2019";
+        data.innerHTML = "31/03/2019";
         title.innerHTML = "Downlaod Android";
-        text.innerHTML = "Lançamos o site na verção de aplicativo android - <a href='https://drive.google.com/file/d/1N_cDg3erO02tWqmCT2CKle04fv13F7qa/view?usp=sharing'>Downlaod</a>"
+        text.innerHTML = "O site também está na versão de aplicativo android <a href='https://drive.google.com/file/d/1N_cDg3erO02tWqmCT2CKle04fv13F7qa/view?usp=sharing'>Downlaod</a>"
     } else {
         data.innerHTML = "30/03/2019";
         title.innerHTML = "New Title";
