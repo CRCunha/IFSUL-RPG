c7fa8d521fc0296a85c84a0c26afedb495f248d2
Cristian Raffi Cunha <cristianraffi@bol.com.br>
2019-04-01 07:33:06 -0300

    Fix all bugs

--- JS/ALERT/alert.js
+++ JS/ALERT/alert.js
@@ -1,3 +1,3 @@
-if (window.innerWidth > 450) {
+if (window.innerWidth > 1300) {
     alert("Para visualizar, utilize um dispositivo mobile, ou abra em modo desenvolvedor (F12) e logo após ative o modo mobile Ctrl+Shift+M .");
 }
